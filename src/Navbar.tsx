e2a07185c7955769f2209d5628044b9fc46e9239
Roman <romalupan89@gmail.com>
2023-06-07 17:48:19 +0300

    changes

--- src/Navbar.tsx
+++ src/Navbar.tsx
@@ -11,34 +11,36 @@ const Navbar = ({ counter, openCart, itemInCart }) => {
     // TODO Add transition to the mobile menu
     <>
       <div className="nav-container">
-        <div className="left-side">
-          <GiHamburgerMenu className="open-menu" onClick={openMenu} />
-          <h2>sneakers</h2>
-        </div>
-        <div className="nav-links" id="nav-links">
-          <GrFormClose className="close-menu" onClick={openMenu} />
-          <a href="">Collections</a>
-          <a href="">Men</a>
-          <a href="">Women</a>
-          <a href="">About</a>
-          <a href="">Contact</a>
-        </div>
-        <div className="profile">
-          <div className="cart-container" onClick={openCart}>
-            <TfiShoppingCart className="cart" />
-            {itemInCart == false ? (
-              <span className="qty">0</span>
-            ) : (
-              <span className="qty">{counter}</span>
-            )}
+        <div className='nav-wrapper'>
+          <div className="left-side">
+            <GiHamburgerMenu className="open-menu" onClick={openMenu} />
+            <h2>sneakers</h2>
+            <div className="nav-links" id="nav-links">
+              <GrFormClose className="close-menu" onClick={openMenu} />
+              <a href="">Collections</a>
+              <a href="">Men</a>
+              <a href="">Women</a>
+              <a href="">About</a>
+              <a href="">Contact</a>
+            </div>
           </div>
-          <img
-            src="image-avatar.png"
-            alt="A profile image"
-            width={20}
-            className="profile-pic"
-          />
+          <div className="profile">
+            <div className="cart-container" onClick={openCart}>
+              <TfiShoppingCart className="cart" />
+              {itemInCart == false ? (
+                <span className="qty">0</span>
+              ) : (
+                <span className="qty">{counter}</span>
+              )}
+            </div>
+            <img
+              src="image-avatar.png"
+              alt="A profile image"
+              className="profile-pic"
+            />
+          </div>
         </div>
+        <hr />
       </div>
     </>
   );
